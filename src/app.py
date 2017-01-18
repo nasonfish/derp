482a0a62490d2acce7573f119bd662830244d532
dellswor <dellswor@cs.uoregon.edu>
2017-01-17 20:29:49 -0800

    latest version of the derp sources

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask.ext.github import GitHub
 from CONFIG import HOST, PORT, DEBUG, APP_SECRET_KEY, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, DB_LOCATION
+from db_helper import UTC_OFFSET
 import psycopg2
 import datetime
 from ast import literal_eval
@@ -25,25 +26,25 @@ github = GitHub(app)
 conn = psycopg2.connect(DB_LOCATION)
 cur  = conn.cursor()
 
+# jinja2 format functions
+@app.template_filter('monday')
+def format_monday(dt):
+    """
+    Input: datetime object
+    Output: date object representing the monday preceding the input datetime
+    """
+    return (dt - datetime.timedelta(days = dt.weekday())).date()
+
+@app.template_filter('to_date')
+def format_date(dt):
+    """
+    Input: datetime object
+    Output: date object
+    """
+    return dt.date()
+
+
 # helper functions
-def dequote(s):
-    """
-    If a string has single or double quotes around it, remove them.
-    Make sure the pair of quotes match.
-    If a matching pair of quotes is not found, return the string unchanged.
-    """
-    if (s[0] == s[-1]) and s.startswith(("'", '"')):
-        return s[1:-1]
-    return s
-
-def make_tuple(tuple_string):
-    """
-    Builds a tuple from a quoted tuple. This is more complicated than it needs to be
-    because psycopg2 will return unquoted strings for single word results and double
-    quoted strings for whitespace delimited results.
-    """
-    return tuple(dequote(str(x)) for x in tuple_string[1:-1].split(','))
-
 def is_login_ok():
     """
     Check that a login is sensible. Should be checked before rendering any content.
@@ -53,7 +54,7 @@ def is_login_ok():
         return False
     if 'duck_id' not in session:
         return False
-        
+
     SQL = "SELECT duck_id,github_username FROM users WHERE github_username=%s and duck_id=%s;"
     data = (session['github_username'],session['duck_id'])
     cur.execute(SQL, data)
@@ -61,7 +62,68 @@ def is_login_ok():
     if db_row is None:
         return False
     return True
-    
+
+def check_daily():
+    """
+    Check whether the user has submitted a daily status today, where 'today' is defined
+    as the 24-hour period stretching from the prior 0600 to the next 0600.
+    """
+    # Select only the records for today
+    SQL = """SELECT create_dt::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'PST'
+FROM dailies
+WHERE date_trunc('day',(now()-interval '6 hours')::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'PST') = date_trunc('day',(create_dt-interval '6 hours')::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'PST')
+and user_fk=%s"""
+    data = (session['user_pk'],)
+    cur.execute(SQL, data)
+    db_row = cur.fetchone()
+    if db_row is None:
+        session['daily_completed'] = False
+    else:
+        session['daily_completed'] = True
+
+
+def check_weekly():
+    """
+    Check whether the user has submitted a weekly status this week, where 'this week'
+    is defined as the period stretching from the prior 0600 Monday to the next 0600 Monday.
+    """
+    # Select only the records for this week
+    SQL = """SELECT create_dt::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'PST'
+FROM weeklies
+WHERE date_trunc('week',(now()-interval '6 hours')::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'PST') = date_trunc('week',(create_dt-interval '6 hours')::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'PST')
+and user_fk=%s"""
+    data = (session['user_pk'],)
+    cur.execute(SQL, data)
+    db_row = cur.fetchone()
+    if db_row is None:
+        session['weekly_completed'] = False
+    else:
+        session['weekly_completed'] = True
+
+
+def check_pending_reviews():
+    """
+    Check whether the user has pending code reviews to complete. Puts the pending review
+    information into the session.
+    """
+    SQL = "SELECT review_pk, reviewee_fk FROM code_reviews WHERE (reviewer_fk = %s AND review_dt IS NULL);"
+    data = (session['user_pk'],)
+    cur.execute(SQL, data)
+    res = cur.fetchall()
+    app.logger.debug("user pending code reviews query: {}".format(str(res)))
+    pending_reviews = []
+    for r in res:
+        pending_reviews.append( dict(zip(('review_pk', 'reviewee_fk'), r)) )
+    for entry in pending_reviews:
+        SQL = "SELECT repo FROM users WHERE user_pk = %s;"
+        data = (entry['reviewee_fk'],)
+        cur.execute(SQL, data)
+        res = cur.fetchone()[0]
+        entry['url'] = res
+    session['pending_reviews'] = pending_reviews
+    app.logger.debug("pending reviews session: {}".format(session['pending_reviews']))
+
+
 # Set "homepage" to index.html
 @app.route('/')
 @app.route('/index')
@@ -130,22 +192,22 @@ def authorized(access_token):
     # Add github information to the session
     session['github_access_token'] = access_token
     session['github_username'] = github.get('user')['login']
-    
+
     # Check for the duck_id based on the github username
     SQL = "SELECT duck_id FROM users WHERE github_username=%s;"
     data = (session['github_username'],)
     cur.execute(SQL, data)
     db_row = cur.fetchone()
-    
+
     if db_row is None:
         # No user registered in the DB... Go to the signup page.
         return redirect(url_for('signup'))
-    
+
     db_duck_id = db_row[0]
     app.logger.debug("db duck_id: {}".format(db_duck_id))
     session['logged_in'] = db_duck_id
     session['duck_id']   = db_duck_id
-    
+
     return redirect(url_for("dashboard"))
 
 
@@ -155,8 +217,8 @@ def dashboard():
     if not is_login_ok():
         msg = "login failure: something is wrong with the session ... try logging in again"
         return redirect(url_for("logout", logout_message = msg))
-        
-    
+
+
     # put the user's id from the db into the session
     #   NOTE: all other methods assume that the user_pk has been put into the session
     if 'user_pk' not in session:
@@ -168,34 +230,13 @@ def dashboard():
         session['user_pk'] = user_pk
 
     # check if the user has submitted a daily status update
-    SQL = "SELECT create_dt FROM dailies WHERE user_fk = %s;"
-    data = (session['user_pk'],)
-    cur.execute(SQL, data)
-    timestamps = list(map(lambda t: t[0], cur.fetchall()))
-    if timestamps:
-        latest_daily_ts = max(timestamps)
-        app.logger.debug("latest user daily: " + str(latest_daily_ts))
-        current_time = datetime.datetime.now()
-        session['daily_completed'] = latest_daily_ts.year == current_time.year and latest_daily_ts.month == current_time.month and latest_daily_ts.day == current_time.day
-        app.logger.debug("user completed daily: " + str(session['daily_completed']))
-    else:
-        session['daily_completed'] = False
+    check_daily()
 
     # check if the user has submitted a weekly status update
-    SQL = "SELECT create_dt FROM weeklies WHERE user_fk = %s;"
-    data = (session['user_pk'],)
-    cur.execute(SQL, data)
-    timestamps = list(map(lambda t: t[0], cur.fetchall()))
-    if timestamps:
-        latest_weekly_ts = max(timestamps)
-        latest_weekly_wk = latest_weekly_ts.isocalendar()[1]
-        app.logger.debug("latest user weekly: " + str(latest_daily_ts))
-        current_time = datetime.datetime.now()
-        current_wk = current_time.isocalendar()[1]
-        session['weekly_completed'] = latest_weekly_wk == current_wk
-        app.logger.debug("user completed weekly: " + str(session['weekly_completed']))
-    else:
-        session['weekly_completed'] = False
+    check_weekly()
+
+    # check for pending code reviews
+    check_pending_reviews()
 
     # display the dashboard page
     return render_template('dashboard.html')
@@ -207,11 +248,11 @@ def profile():
     if not is_login_ok():
         msg = "login failure: something is wrong with the session ... try logging in again"
         return redirect(url_for("logout", logout_message = msg))
-        
+
     # if we are getting a POST, handle it
     if request.method == 'POST':
 
-        # handle daily status submission
+        # handle profile info submission
         if 'repo' in request.form and 'email' in request.form:
             repo_msg = request.form['repo']
             email_msg = request.form['email']
@@ -222,11 +263,11 @@ def profile():
             conn.commit()
 
     # get the user's info from the db
-    SQL = "SELECT (github_username, duck_id, email, repo) FROM users WHERE duck_id = %s;"
+    SQL = "SELECT github_username, duck_id, email, repo FROM users WHERE duck_id = %s;"
     data = (session['duck_id'],)
     cur.execute(SQL, data)
     res = cur.fetchone()
-    session['user_info'] = dict(zip(('github_username', 'duck_id', 'email', 'repo'), make_tuple(res[0])))
+    session['user_info'] = dict(zip(('github_username', 'duck_id', 'email', 'repo'), res))
     app.logger.debug("setting user_info: {}".format(str(session['user_info'])))
 
     # in any event, display the profile page
@@ -241,18 +282,7 @@ def daily():
         return redirect(url_for("logout", logout_message = msg))
 
     # check if the user has submitted a daily status update
-    SQL = "SELECT create_dt FROM dailies WHERE user_fk = %s;"
-    data = (session['user_pk'],)
-    cur.execute(SQL, data)
-    timestamps = list(map(lambda t: t[0], cur.fetchall()))
-    if timestamps:
-        latest_daily_ts = max(timestamps)
-        app.logger.debug("latest user daily: " + str(latest_daily_ts))
-        current_time = datetime.datetime.now()
-        session['daily_completed'] = latest_daily_ts.year == current_time.year and latest_daily_ts.month == current_time.month and latest_daily_ts.day == current_time.day
-        app.logger.debug("user completed daily: " + str(session['daily_completed']))
-    else:
-        session['daily_completed'] = False
+    check_daily()
 
     # if we are getting a POST, handle it
     if request.method == 'POST':
@@ -266,7 +296,7 @@ def daily():
 
             if not session['daily_completed']:
                 SQL = "INSERT INTO dailies (user_fk, create_dt, message) VALUES (%s, %s, %s);"
-                data = (session['user_pk'], datetime.datetime.now(), message)
+                data = (session['user_pk'], datetime.datetime.utcnow(), message)
                 cur.execute(SQL, data)
                 conn.commit()
                 session['daily_completed'] = True
@@ -280,16 +310,18 @@ def daily():
     # build the report
     #   note: this could be done in the same query as above, but this seems safer
     #         in case the tables change
-    SQL = "SELECT (create_dt, message) FROM dailies WHERE user_fk = %s;"
+    SQL = "SELECT (create_dt-interval '6 hours')::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'PST', message FROM dailies WHERE user_fk = %s;"
     data = (session['user_pk'],)
     cur.execute(SQL, data)
     res = cur.fetchall()
-    app.logger.debug("user daily entries: {}".format(str(res)))
+    #app.logger.debug("user daily entries: {}".format(str(res)))
     daily_report = []
     for r in res:
-        daily_report.append( dict(zip(('timestamp', 'status_message'), make_tuple(r[0]))) )
-    for entry in daily_report:
-        entry['timestamp'] = entry['timestamp'].split()[0]
+        e = dict()
+        e['timestamp']=r[0]
+        e['status_message']=r[1]
+        daily_report.append(e)
+
     daily_report.sort(reverse=True, key=lambda entry: entry['timestamp'])
     session['daily_report'] = daily_report
     app.logger.debug("user daily entries session object: {}".format(str(daily_report)))
@@ -304,22 +336,9 @@ def weekly():
     if not is_login_ok():
         msg = "login failure: something is wrong with the session ... try logging in again"
         return redirect(url_for("logout", logout_message = msg))
-    
+
     # check if the user has submitted a weekly status update
-    SQL = "SELECT create_dt FROM weeklies WHERE user_fk = %s;"
-    data = (session['user_pk'],)
-    cur.execute(SQL, data)
-    timestamps = list(map(lambda t: t[0], cur.fetchall()))
-    if timestamps:
-        latest_weekly_ts = max(timestamps)
-        latest_weekly_wk = latest_weekly_ts.isocalendar()[1]
-        app.logger.debug("latest user weekly: " + str(latest_weekly_ts))
-        current_time = datetime.datetime.now()
-        current_wk = current_time.isocalendar()[1]
-        session['weekly_completed'] = latest_weekly_wk == current_wk
-        app.logger.debug("user completed weekly: " + str(session['weekly_completed']))
-    else:
-        session['weekly_completed'] = False
+    check_weekly()
 
     # if we are getting a POST, handle it
     if request.method == 'POST':
@@ -333,7 +352,7 @@ def weekly():
 
             if not session['weekly_completed']:
                 SQL = "INSERT INTO weeklies (user_fk, create_dt, accomplishments, challenges, next_steps, comments) VALUES (%s, %s, %s, %s, %s, %s);"
-                data = (session['user_pk'], datetime.datetime.now(), accomplishments_msg, challenges_msg, next_steps_msg, comments_msg)
+                data = (session['user_pk'], datetime.datetime.utcnow(), accomplishments_msg, challenges_msg, next_steps_msg, comments_msg)
                 cur.execute(SQL, data)
                 conn.commit()
                 session['weekly_completed'] = True
@@ -346,24 +365,72 @@ def weekly():
     # build the report
     #   note: this could be done in the same query as above, but this seems safer
     #         in case the tables change
-    SQL = "SELECT (create_dt, accomplishments, challenges, next_steps, comments) FROM weeklies WHERE user_fk = %s;"
+    SQL = "SELECT (create_dt-interval '6 hours')::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'PST', accomplishments, challenges, next_steps, comments FROM weeklies WHERE user_fk = %s;"
     data = (session['user_pk'],)
     cur.execute(SQL, data)
     res = cur.fetchall()
     app.logger.debug("user weekly entries: {}".format(str(res)))
     weekly_report = []
     for r in res:
-        weekly_report.append( dict(zip(('timestamp', 'accomplishments', 'challenges', 'next_steps', 'comments'), make_tuple(r[0]))) )
-    for entry in weekly_report:
-        dt = datetime.datetime.strptime(entry['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
-        monday_of_this_week = dt - datetime.timedelta(days = datetime.datetime.now().weekday())
-        entry['timestamp'] = monday_of_this_week.date()
+        e = dict()
+        e['timestamp']=r[0]
+        e['accomplishments']=r[1]
+        e['challenges']=r[2]
+        e['next_steps']=r[3]
+        e['comments']=r[4]
+        weekly_report.append(e)
     weekly_report.sort(reverse=True, key=lambda entry: entry['timestamp'])
     session['weekly_report'] = weekly_report
     app.logger.debug("user weekly entries session object: {}".format(str(weekly_report)))
 
     # in any event, display the weekly page
     return render_template('weekly.html')
+
+
+# display code review
+@app.route('/code_review', methods=['GET', 'POST'])
+def code_review():
+    if not is_login_ok():
+        msg = "login failure: something is wrong with the session ... try logging in again"
+        return redirect(url_for("logout", logout_message = msg))
+
+    # if we are getting a POST, handle it
+    if request.method == 'POST':
+
+        # handle weekly status submission:
+        #   - strip the review_pk off the form name
+        #   - update the db with the current timestamp
+        review_pk = None
+        comments_msg = None
+        for key in request.form:
+            if key.startswith('review.'):
+                review_pk = key.partition('.')[-1]
+                comments_msg = request.form[key]
+
+        if review_pk is not None and comments_msg is not None:
+            SQL = "UPDATE code_reviews SET review_dt=%s, comments=%s WHERE (review_pk=%s);"
+            data = (datetime.datetime.utcnow(), comments_msg, review_pk)
+            cur.execute(SQL, data)
+            conn.commit()
+
+    # check for pending code reviews
+    check_pending_reviews()
+
+    # build the report
+    SQL = "SELECT review_dt, comments FROM code_reviews WHERE (reviewee_fk = %s AND released = true);"
+    data = (session['user_pk'],)
+    cur.execute(SQL, data)
+    res = cur.fetchall()
+    app.logger.debug("user code review entries: {}".format(str(res)))
+    review_report = []
+    for r in res:
+        review_report.append( dict(zip(('timestamp', 'comments'), r)) )
+    review_report.sort(reverse=True, key=lambda entry: entry['timestamp'])
+    session['review_report'] = review_report
+    app.logger.debug("code review report session object: {}".format(str(review_report)))
+
+    # in any event, display the weekly page
+    return render_template('code_review.html')
 
 
 # display signup page
--- src/db_helper.py
+++ src/db_helper.py
@@ -5,7 +5,8 @@ import datetime
 
 # note: this is a gross hack for converting utc to local ... platform dependent and incorrect
 #       in general ... do not assume that this will work without checking that it does!
-TZ_OFFSET = datetime.datetime.now() - datetime.datetime.utcnow()
+# UTC_OFFSET = datetime.datetime.now() - datetime.datetime.utcnow()
+UTC_OFFSET = datetime.timedelta(hours = 8)  # worse hack ... difference between PT and UTC
 
 
 # TODO: wrap db queries into functions
